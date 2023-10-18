03497988083a98b3ec43b4c7800545e5aeb2fe86
rafaelarabelobarbosa@outlook.comnvm alias default 6.11.5nvm alias default 6.11.5nvm alias default 6.11.5 git config --global user.name rafaelarabelobarbosa@outlook.com git config --global user.name rafaelarabelobarbosa@outlook.com <rafaelarabelobarbosa@outlook.com>
2023-10-18 16:36:25 -0300

    atualizo o nivel do heroi

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-let nome = "Elfa36"
-let xp = 10001
-let nivel = ""
+    while (true){
 
-if (xp <= 1000) {
-  nivel = "Ferro"
-} else if (xp <= 2000) {
-  nivel = "Bronze"
-} else if (xp <= 6000) {
-  nivel = "Prata"
-} else if (xp <= 7000) {
-  nivel = "Ouro"
-} else if (xp <= 8000) {
-  nivel = "Platina"
-} else if (xp <= 9000) {
-  nivel = "Ascendente"
-} else if (xp <= 10000) {
-  nivel = "Imortal"
-} else if (xp >= 10001) {
-  nivel = "Radiante"
+    let heroi = 'Elfa36'
+    let  XP = 10000
+    let nivel = ["ferro", "bronze", "prata", "ouro", "platina", "ascendente", "imortal", "radiante"]
+    
+    if(XP < 1000){
+        console.log("O herói " + heroi + " está no nível " + nivel[0] )
+    }
+    
+     else if((XP >= 1001) && (XP <= 2000)){
+        console.log("O herói " + heroi + " está no nível " + nivel[1])
+    }
+    
+    else if((XP >= 2001) && (XP <= 5000)){
+        console.log("O herói " + heroi + " está no nível " + nivel[2])
+    }
+    
+    else if((XP >= 5001) && (XP <= 7000)){
+        console.log("O herói " + heroi + " está no nível " + nivel[3])
+    }
+    
+    else if((XP >= 7001) && (XP <= 8000)){
+        console.log("O herói " + heroi + " está no nível " + nivel[4])
+    }
+    else if((XP >= 8001) && (XP <= 9000)){
+        console.log("O herói " + heroi + " está no nível " + nivel[5])
+    }
+    else if((XP >= 9001) && (XP <= 10000)){
+        console.log("O herói " + heroi + " está no nível " + nivel[6])
+    }
+    else{
+        console.log("O herói " + heroi + " está no nível de" + nivel[7])
+    }
+ 
+break
+
 }
-
-console.log('O Herói de nome ${nome} está no nível de ${nivel}')
